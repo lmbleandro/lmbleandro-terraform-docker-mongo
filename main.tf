9c8c55745ca551cff50451e3ae5a4844ca4bfae9
Leandro Basilio <leandro.basilio@cortex-intelligence.com>
2022-12-26 12:09:56 -0300

    gitignore

--- main.tf
+++ main.tf
@@ -1,15 +1,8 @@
-# get the mongo docker image
-resource "docker_image" "mongo" {
-  name         = "mongo"
-  keep_locally = true
-}
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-lmbleandro"
 
-# start a container and expose the 27017 port
-resource "docker_container" "mongo" {
-  name  = var.mongo_container_name
-  image = docker_image.mongo.latest
-  ports {
-    internal = 27017
-    external = var.mongo_exposed_port
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
